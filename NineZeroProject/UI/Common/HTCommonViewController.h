70f6014579e9214068808eb2916a88f8bdd9fcbc
huteng <408895175@qq.com>
2015-11-24 01:08:58 +0800

    公共基类

---
+++ NineZeroProject/UI/Common/HTCommonViewController.h
@@ -0,0 +1,13 @@
+//
+//  HTCommonViewController.h
+//  NineZeroProject
+//
+//  Created by ronhu on 15/11/24.
+//  Copyright © 2015年 ronhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTCommonViewController : UIViewController
+
+@end
---
+++ NineZeroProject/UI/Common/HTCommonViewController.m
@@ -0,0 +1,13 @@
+//
+//  HTCommonViewController.m
+//  NineZeroProject
+//
+//  Created by ronhu on 15/11/24.
+//  Copyright © 2015年 ronhu. All rights reserved.
+//
+
+#import "HTCommonViewController.h"
+
+@implementation HTCommonViewController
+
+@end
