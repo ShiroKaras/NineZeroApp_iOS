7b1c0bb0695189329849d9e50069746ab2450104
huteng <408895175@qq.com>
2015-11-24 01:05:55 +0800

    登录判断

--- NineZeroProject/UI/Login/HTLoginRootController.m
+++ NineZeroProject/UI/Login/HTLoginRootController.m
@@ -10,11 +10,14 @@
 #import "CommonUI.h"
 #import "HTRegisterController.h"
 #import "HTLoginController.h"
+#import <MBProgressHUD+BWMExtension.h>
 
-@interface HTLoginRootController ()
+@interface HTLoginRootController () <UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *userPasswordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *registerButton;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
 
 @end
 
@@ -23,8 +26,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"主界面";
+    
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewDidTap)];
     [self.view addGestureRecognizer:tapGesture];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textFieldTextDidChange:) name:UITextFieldTextDidChangeNotification object:nil];
+    
+    _registerButton.enabled = NO;
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -37,6 +49,31 @@
     self.navigationController.navigationBarHidden = NO;    
 }
 
+#pragma mark - UITextField
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField == self.userNameTextField) {
+        [self.userPasswordTextField becomeFirstResponder];
+        return YES;
+    }
+    if (textField == self.userNameTextField) {
+        if ([self isRegisterButtonValid]) {
+            [self registerButtonClicked:nil];
+            return YES;
+        }
+        return NO;
+    }
+    return YES;
+}
+
+- (void)textFieldTextDidChange:(NSNotification *)notification {
+    if ([self isRegisterButtonValid]) {
+        self.registerButton.enabled = YES;
+    } else {
+        self.registerButton.enabled = NO;
+    }
+}
+
 #pragma mark - Action
 
 - (void)viewDidTap {
@@ -44,6 +81,10 @@
 }
 
 - (IBAction)registerButtonClicked:(UIButton *)sender {
+    if (self.userNameTextField.text.length != 11) {
+        [MBProgressHUD bwm_showTitle:@"手机号码位数不正确" toView:self.view hideAfter:1.0 msgType:BWMMBProgressHUDMsgTypeWarning];
+        return;
+    }
     HTRegisterController *registerController = [[HTRegisterController alloc] init];
     [self.navigationController pushViewController:registerController animated:YES]; 
 }
@@ -51,6 +92,16 @@
 - (IBAction)loginButtonClicked:(UIButton *)sender {
     HTLoginController *loginController = [[HTLoginController alloc] init];
     [self.navigationController pushViewController:loginController animated:YES];
+}
+
+#pragma mark - Tool Method
+
+- (BOOL)isRegisterButtonValid {
+    if (self.userNameTextField.text.length != 0 &&
+        self.userPasswordTextField.text.length != 0) {
+        return YES;
+    }
+    return NO;
 }
 
 @end
