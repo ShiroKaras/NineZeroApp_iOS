7d4a8155627441964f2e6e3bfb2ac03219cca149
huteng <408895175@qq.com>
2016-01-31 16:33:16 +0800

    添加一个文件

---
+++ NineZeroProject/UI/Mascot/HTMascotIntroController.m
@@ -0,0 +1,22 @@
+//
+//  HTMascotIntroController.m
+//  NineZeroProject
+//
+//  Created by ronhu on 16/1/31.
+//  Copyright © 2016年 ronhu. All rights reserved.
+//
+
+#import "HTMascotIntroController.h"
+
+@interface HTMascotIntroController ()
+
+@end
+
+@implementation HTMascotIntroController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
