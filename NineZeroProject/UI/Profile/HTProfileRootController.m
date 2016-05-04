969eaae6fdbdffb31cf4cd1757053a7133424b76
SinLemon <shirokaras@qq.com>
2016-05-04 17:53:10 +0800

    Just in case

--- NineZeroProject/UI/Profile/HTProfileRootController.m
+++ NineZeroProject/UI/Profile/HTProfileRootController.m
@@ -191,8 +191,10 @@
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     HTProfileRecordCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([HTProfileRecordCell class]) forIndexPath:indexPath];
-    DLog(@"answerList->%@", _profileInfo.answer_list[indexPath.row].question_video_cover);
-    [cell.coverImageView sd_setImageWithURL:_profileInfo.answer_list[indexPath.row].question_video_cover placeholderImage:[UIImage imageNamed:@"img_monday_music_cover_default"]];
+    if (_profileInfo.answer_list.count != 0) {
+        DLog(@"answerList->%@", _profileInfo.answer_list[indexPath.row].question_video_cover);
+        [cell.coverImageView sd_setImageWithURL:_profileInfo.answer_list[indexPath.row].question_video_cover placeholderImage:[UIImage imageNamed:@"img_monday_music_cover_default"]];
+    }
     cell.delegate = self;
     cell.indexPath = indexPath;
     return cell;
