0fd124b34186cd91b05351746500b88d14d25ac5
SinLemon <shirokaras@qq.com>
2016-08-09 19:39:42 +0800

    Update - top

--- NineZeroProject/UI/Login/Question/HTComposeView.m
+++ NineZeroProject/UI/Login/Question/HTComposeView.m
@@ -123,12 +123,12 @@
     
     // 1.1 参与者
     [_participatorView mas_makeConstraints:^(MASConstraintMaker *make) {
-        if (IPHONE5_SCREEN_WIDTH) {
+        if (SCREEN_WIDTH <= IPHONE5_SCREEN_WIDTH) {
             make.top.equalTo(_dimmingView).offset(42);
-        } else if (IPHONE6_SCREEN_WIDTH) {
-            make.top.equalTo(_dimmingView).offset(54);
-        } else if (IPHONE6_PLUS_SCREEN_WIDTH) {
+        } else if (SCREEN_WIDTH >= IPHONE6_PLUS_SCREEN_WIDTH) {
             make.top.equalTo(_dimmingView).offset(74);
+        } else {
+            make.top.equalTo(_dimmingView).offset(54);
         }
         make.left.equalTo(_dimmingView);
         make.right.equalTo(_dimmingView);
