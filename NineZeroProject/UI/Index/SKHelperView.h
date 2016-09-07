c089f479b9789485508b0231f8eeb9e40b0091c1
SinLemon <shirokaras@qq.com>
2016-09-07 20:04:26 +0800

    Add - Image resource

---
+++ NineZeroProject/UI/Index/SKHelperView.h
@@ -0,0 +1,13 @@
+//
+//  SKHelperView.h
+//  NineZeroProject
+//
+//  Created by SinLemon on 16/9/7.
+//  Copyright © 2016年 ronhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKHelperView : UIView
+
+@end
---
+++ NineZeroProject/UI/Index/SKHelperView.m
@@ -0,0 +1,21 @@
+//
+//  SKHelperView.m
+//  NineZeroProject
+//
+//  Created by SinLemon on 16/9/7.
+//  Copyright © 2016年 ronhu. All rights reserved.
+//
+
+#import "SKHelperView.h"
+
+@implementation SKHelperView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
