f9981a5c033895c2bd1bbe25208d18a7220e5bb9
SinLemon <shirokaras@qq.com>
2016-09-12 21:15:52 +0800

    Update - Black and white

--- NineZeroProject/UI/Index/SKQuestionPageViewController.m
+++ NineZeroProject/UI/Index/SKQuestionPageViewController.m
@@ -101,7 +101,45 @@
         int j = floor(itemInPage/3);
         UIView *itemView = [[UIView alloc] initWithFrame:CGRectMake(ROUND_WIDTH_FLOAT(35)+SCREEN_WIDTH*pageNumber+i*ROUND_WIDTH_FLOAT(93), ROUND_WIDTH_FLOAT(90)*j, ROUND_WIDTH_FLOAT(64), ROUND_WIDTH_FLOAT(64))];
         UIImageView *coverImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, itemView.width, itemView.height)];
-        [coverImageView sd_setImageWithURL:[NSURL URLWithString:questionList[questionNumber].question_video_cover] placeholderImage:nil];
+        
+        [coverImageView sd_setImageWithURL:[NSURL URLWithString:questionList[questionNumber].question_video_cover] placeholderImage:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+            int type;
+            if (questionList[questionNumber].isPassed || ![questionList[questionNumber].question_ar_location isEqualToString:@""])  type = 0;
+            else    type = 1;
+            
+            [[SDWebImageManager sharedManager] downloadImageWithURL:imageURL options:SDWebImageRetryFailed progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+                
+                NSString* key = [[SDWebImageManager sharedManager] cacheKeyForURL:imageURL];
+                BOOL result = [[SDImageCache sharedImageCache] diskImageExistsWithKey:key];
+                NSString* imagePath = [[SDImageCache sharedImageCache] defaultCachePathForKey:key];
+                NSData* newData = [NSData dataWithContentsOfFile:imagePath];
+                if (!result || !newData) {
+//                    BOOL imageIsPng = ImageDataHasPNGPreffixWithData(nil);
+                    NSData* imageData = nil;
+//                    if (imageIsPng) {
+//                        imageData = UIImagePNGRepresentation(image);
+//                    }
+//                    else {
+                    imageData = UIImageJPEGRepresentation(image, (CGFloat)1.0);
+//                    }
+                    NSFileManager* _fileManager = [NSFileManager defaultManager];
+                    if (imageData) {
+                        [_fileManager removeItemAtPath:imagePath error:nil];
+                        [_fileManager createFileAtPath:imagePath contents:imageData attributes:nil];
+                    }
+                }
+                newData = [NSData dataWithContentsOfFile:imagePath];
+                UIImage* grayImage = nil;
+                if (type == 0) {
+                    grayImage = [UIImage imageWithData:newData];
+                }else{
+                    UIImage* newImage = [UIImage imageWithData:newData];
+                    
+                    grayImage = [self grayscale:newImage type:1];
+                }
+                coverImageView.image = grayImage;
+            }];
+        }];
         coverImageView.layer.cornerRadius = itemView.width/2;
         coverImageView.layer.masksToBounds = YES;
         [itemView addSubview:coverImageView];
@@ -201,6 +239,82 @@
 
 - (void)didClickCloseButtonInController:(HTPreviewCardController *)controller {
     [controller.navigationController popViewControllerAnimated:YES];
+}
+
+#pragma mark - Tool
+
+- (UIImage*)grayscale:(UIImage*)anImage type:(int)type {
+    CGImageRef imageRef = anImage.CGImage;
+    
+    size_t width  = CGImageGetWidth(imageRef);
+    size_t height = CGImageGetHeight(imageRef);
+    
+    size_t bitsPerComponent = CGImageGetBitsPerComponent(imageRef);
+    size_t bitsPerPixel = CGImageGetBitsPerPixel(imageRef);
+    size_t bytesPerRow = CGImageGetBytesPerRow(imageRef);
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(imageRef);
+    CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(imageRef);
+    
+    bool shouldInterpolate = CGImageGetShouldInterpolate(imageRef);
+    CGColorRenderingIntent intent = CGImageGetRenderingIntent(imageRef);
+    CGDataProviderRef dataProvider = CGImageGetDataProvider(imageRef);
+    CFDataRef data = CGDataProviderCopyData(dataProvider);
+    UInt8 *buffer = (UInt8*)CFDataGetBytePtr(data);
+    
+    NSUInteger  x, y;
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            UInt8 *tmp;
+            tmp = buffer + y * bytesPerRow + x * 4;
+            
+            UInt8 red,green,blue;
+            red = *(tmp + 0);
+            green = *(tmp + 1);
+            blue = *(tmp + 2);
+            
+            UInt8 brightness;
+            switch (type) {
+                case 1:
+                    brightness = (77 * red + 28 * green + 151 * blue) / 256;
+                    *(tmp + 0) = brightness;
+                    *(tmp + 1) = brightness;
+                    *(tmp + 2) = brightness;
+                    break;
+                case 2:
+                    *(tmp + 0) = red;
+                    *(tmp + 1) = green * 0.7;
+                    *(tmp + 2) = blue * 0.4;
+                    break;
+                case 3:
+                    *(tmp + 0) = 255 - red;
+                    *(tmp + 1) = 255 - green;
+                    *(tmp + 2) = 255 - blue;
+                    break;
+                default:
+                    *(tmp + 0) = red;
+                    *(tmp + 1) = green;
+                    *(tmp + 2) = blue;
+                    break;
+            }
+        }
+    }
+    
+    
+    CFDataRef effectedData = CFDataCreate(NULL, buffer, CFDataGetLength(data));
+    CGDataProviderRef effectedDataProvider = CGDataProviderCreateWithCFData(effectedData);
+    CGImageRef effectedCgImage = CGImageCreate(
+                                               width, height,
+                                               bitsPerComponent, bitsPerPixel, bytesPerRow,
+                                               colorSpace, bitmapInfo, effectedDataProvider,
+                                               NULL, shouldInterpolate, intent);
+    
+    UIImage *effectedImage = [[UIImage alloc] initWithCGImage:effectedCgImage];
+    CGImageRelease(effectedCgImage);
+    CFRelease(effectedDataProvider);
+    CFRelease(effectedData);
+    CFRelease(data);
+    
+    return effectedImage;
 }
 
 @end
