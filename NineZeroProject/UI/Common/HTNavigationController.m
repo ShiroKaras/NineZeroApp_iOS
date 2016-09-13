46894dcc999bb3c7e386db1b25e06a6c099cc88a
SinLemon <shirokaras@qq.com>
2016-09-13 19:01:35 +0800

    Update - NavigationBar backButton (no change)

--- NineZeroProject/UI/Common/HTNavigationController.m
+++ NineZeroProject/UI/Common/HTNavigationController.m
@@ -13,17 +13,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     [[UINavigationBar appearance] setBackgroundImage:[UIImage imageFromColor:[UIColor colorWithHex:0x1a1a1a]] forBarPosition:UIBarPositionAny barMetrics:UIBarMetricsDefault];
     [[UINavigationBar appearance] setTranslucent:NO];
     [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
     [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor]}];
-
-
+//    [[UINavigationBar appearance] setTitleVerticalPositionAdjustment:-10.0 forBarMetrics:UIBarMetricsDefault];
+    /*
     //自定义返回按钮
-    UIImage *backButtonImage = [[UIImage imageNamed:@"btn_navi_anchor_left"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 30, 0, 0)];
+    UIImage *backButtonImage = [[UIImage imageNamed:@"btn_profile_back"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
     [[UIBarButtonItem appearance] setBackButtonBackgroundImage:backButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
     //将返回按钮的文字position设置不在屏幕上显示
     [[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(NSIntegerMin, NSIntegerMin) forBarMetrics:UIBarMetricsDefault];
+    [[UIBarButtonItem appearance] setBackButtonBackgroundVerticalPositionAdjustment:-10 forBarMetrics:UIBarMetricsDefault];
+    [[UIBarButtonItem appearance] setBackgroundVerticalPositionAdjustment:-10 forBarMetrics:UIBarMetricsDefault];
+    */
     
 //    UIImage *backImage = [UIImage imageNamed:@"btn_navi_anchor_left"];
 //    [[UINavigationBar appearance] setBackIndicatorImage:backImage];
@@ -41,9 +45,8 @@
     
     if (viewController.navigationItem.leftBarButtonItem== nil && [self.viewControllers count] > 1) {
         UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-        [button setImage:[UIImage imageNamed:@"btn_navi_anchor_left"] forState:UIControlStateNormal];
-        [button setImage:[UIImage imageNamed:@"btn_navi_anchor_left_highlight"] forState:UIControlStateHighlighted];
-        [button sizeToFit];
+        [button setImage:[UIImage imageNamed:@"btn_profile_back"] forState:UIControlStateNormal];
+        [button setImage:[UIImage imageNamed:@"btn_profile_back_highlight"] forState:UIControlStateHighlighted];
         button.width += 10;
         [button addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
         viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
--- NineZeroProject/UI/Profile/HTProfileSettingController.m
+++ NineZeroProject/UI/Profile/HTProfileSettingController.m
@@ -203,6 +203,7 @@ static NSInteger const kChangeNameViewTag = 12345;
     [super viewDidLoad];
     self.tableView.backgroundColor = [UIColor blackColor];
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+//    self.tableView.tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 20)];
     self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 30)];
     
     self.navigationItem.title = @"设置";
@@ -219,12 +220,16 @@ static NSInteger const kChangeNameViewTag = 12345;
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [[UIApplication sharedApplication] setStatusBarHidden:YES];
+//    CGRect rect = self.navigationController.navigationBar.frame;
+//    self.navigationController.navigationBar.frame = CGRectMake (rect.origin.x, rect.origin.y, rect.size.width, 64);
     [MobClick beginLogPageView:@"settingpage"];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     [[UIApplication sharedApplication] setStatusBarHidden:NO];
+//    CGRect rect = self.navigationController.navigationBar.frame;
+//    self.navigationController.navigationBar.frame = CGRectMake (rect.origin.x, rect.origin.y, rect.size.width, 44);
     [MobClick endLogPageView:@"settingpage"];
 }
 
