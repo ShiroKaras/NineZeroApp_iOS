999388f6ad4f5e68eb38b4283b02238055ae4d0f
SinLemon <shirokaras@qq.com>
2016-09-20 18:41:13 +0800

    Remove - history question AlertView

--- NineZeroProject/UI/Question/HTPreviewCardController.m
+++ NineZeroProject/UI/Question/HTPreviewCardController.m
@@ -292,7 +292,6 @@ static CGFloat kItemMargin = 17;         // item之间间隔
     } else if (_cardType == HTPreviewCardTypeHistoryLevel) {
         _timeView = [[HTCardTimeView alloc] initWithFrame:CGRectZero];
         [self.view addSubview:_timeView];
-        [self showAlert];
     }
     
     dispatch_async(dispatch_get_main_queue(), ^{
