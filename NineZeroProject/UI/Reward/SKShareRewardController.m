91f947a50b35dabfd8bec373cfc3210cba728504
SinLemon <shirokaras@qq.com>
2016-10-17 18:21:26 +0800

    Add - ShareRewardController

---
+++ NineZeroProject/UI/Reward/SKShareRewardController.m
@@ -0,0 +1,91 @@
+//
+//  SKShareRewardController.m
+//  NineZeroProject
+//
+//  Created by SinLemon on 2016/10/17.
+//  Copyright © 2016年 ronhu. All rights reserved.
+//
+
+#import "SKShareRewardController.h"
+#import "HTUIHeader.h"
+
+@interface SKShareRewardController ()
+@property (nonatomic, assign) long coinCount;
+@end
+
+@implementation SKShareRewardController
+
+- (instancetype)initWithCoinNumber:(long)coinCount
+{
+    self = [super init];
+    if (self) {
+        _coinCount = coinCount;
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIImageView *headerImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"img_sharepage_share3"]];
+    [headerImageView sizeToFit];
+    [self.view addSubview:headerImageView];
+    
+    UIImageView *getImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"img_sharepage_share1"]];
+    [getImageView sizeToFit];
+    [self.view addSubview:getImageView];
+    
+    UIImageView *coinImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"img_sharepage_share2"]];
+    [coinImageView sizeToFit];
+    [self.view addSubview:coinImageView];
+    
+    UILabel *coinCountLabel = [[UILabel alloc] init];
+    coinCountLabel.text = @"2";
+    //coinCountLabel.text = [NSString stringWithFormat:@"%ld", _coinCount];
+    coinCountLabel.textColor = [UIColor colorWithHex:0xED203B];
+    coinCountLabel.font = MOON_FONT_OF_SIZE(19);
+    [coinCountLabel sizeToFit];
+    [self.view addSubview:coinCountLabel];
+    
+    UIButton *sureButton = [HTLoginButton buttonWithType:UIButtonTypeCustom];
+    [sureButton setTitle:@"确定" forState:UIControlStateNormal];
+    sureButton.titleLabel.font = [UIFont fontWithName:@"PingFangSC-Regular" size:18];
+    sureButton.enabled = YES;
+    [sureButton addTarget:self action:@selector(onClickSureButton) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:sureButton];
+    
+    __weak __typeof(self)weakSelf = self;
+    
+    [headerImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(weakSelf).offset(ROUND_HEIGHT_FLOAT(143));
+        make.centerX.equalTo(weakSelf);
+    }];
+    
+    [getImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(headerImageView);
+        make.top.equalTo(headerImageView).offset(20);
+    }];
+    
+    [coinCountLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.equalTo(@12);
+        make.centerY.equalTo(getImageView);
+        make.left.equalTo(getImageView.mas_right);
+    }];
+    
+    [coinImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(coinCountLabel.mas_right);
+        make.centerY.equalTo(coinCountLabel);
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)onClickSureButton {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
