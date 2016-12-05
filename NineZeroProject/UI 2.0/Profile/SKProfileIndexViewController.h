beee866f409b6d8872bf437ddf44988384adbe15
SinLemon <shirokaras@qq.com>
2016-12-05 10:38:05 +0800

    Add - Profile images

---
+++ NineZeroProject/UI 2.0/Profile/SKProfileIndexViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKProfileIndexViewController.h
+//  NineZeroProject
+//
+//  Created by SinLemon on 2016/12/5.
+//  Copyright © 2016年 ronhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKProfileIndexViewController : UIViewController
+
+@end
---
+++ NineZeroProject/UI 2.0/Profile/SKProfileIndexViewController.m
@@ -0,0 +1,37 @@
+//
+//  SKProfileIndexViewController.m
+//  NineZeroProject
+//
+//  Created by SinLemon on 2016/12/5.
+//  Copyright © 2016年 ronhu. All rights reserved.
+//
+
+#import "SKProfileIndexViewController.h"
+
+@interface SKProfileIndexViewController ()
+
+@end
+
+@implementation SKProfileIndexViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
