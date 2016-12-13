6e0a9f98b9aa83ba1bc1d6a937a64907638ab84c
SinLemon <shirokaras@qq.com>
2016-12-13 16:29:32 +0800

    Update - Images

---
+++ NineZeroProject/Service 2.0/SKCommonService.h
@@ -0,0 +1,13 @@
+//
+//  SKCommonService.h
+//  NineZeroProject
+//
+//  Created by SinLemon on 2016/12/13.
+//  Copyright © 2016年 ronhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKCommonService : NSObject
+
+@end
---
+++ NineZeroProject/Service 2.0/SKCommonService.m
@@ -0,0 +1,13 @@
+//
+//  SKCommonService.m
+//  NineZeroProject
+//
+//  Created by SinLemon on 2016/12/13.
+//  Copyright © 2016年 ronhu. All rights reserved.
+//
+
+#import "SKCommonService.h"
+
+@implementation SKCommonService
+
+@end
