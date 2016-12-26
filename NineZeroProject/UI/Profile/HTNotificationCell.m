bf1b2f50c9c6e953f81499739fa0a3e28c461202
SinLemon <shirokaras@qq.com>
2016-12-26 11:35:47 +0800

    Update - Notification cell

--- NineZeroProject/UI/Profile/HTNotificationCell.m
+++ NineZeroProject/UI/Profile/HTNotificationCell.m
@@ -15,6 +15,7 @@
 static CGFloat kLineSpace = 7;
 
 @interface HTNotificationCell ()
+@property (nonatomic, strong) UIView *backView;
 @property (nonatomic, strong) UIImageView *avatar;
 @property (nonatomic, strong) UILabel *nameLabel;
 @property (nonatomic, strong) UILabel *timeLabel;
@@ -27,6 +28,24 @@ static CGFloat kLineSpace = 7;
     if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
         self.backgroundColor = [UIColor blackColor];
         self.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+        _backView = [[UIView alloc] initWithFrame:CGRectMake(12, 0, SCREEN_WIDTH-24, 20)];
+        _backView.backgroundColor = COMMON_SEPARATOR_COLOR;
+        _backView.layer.cornerRadius = 5;
+        [self.contentView addSubview:_backView];
+        
+        _contentLabel = [[TTTAttributedLabel alloc] initWithFrame:CGRectZero];
+        _contentLabel.font = [UIFont systemFontOfSize:13];
+        _contentLabel.textColor = [UIColor colorWithHex:0x9c9c9c];
+        _contentLabel.numberOfLines = 0;
+        _contentLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        _contentLabel.lineSpacing = kLineSpace;
+        [self.contentView addSubview:_contentLabel];
+        
+        _separator = [[UIView alloc] initWithFrame:CGRectMake(24, 0, SCREEN_WIDTH-48, 1)];
+        _separator.backgroundColor = [UIColor colorWithHex:0x2d2d2d];
+        [self.contentView addSubview:_separator];
+        
         _avatar = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"img_profile_notification_mascot1"]];
         [self.contentView addSubview:_avatar];
         
@@ -39,18 +58,6 @@ static CGFloat kLineSpace = 7;
         _timeLabel.font = [UIFont systemFontOfSize:11];
         _timeLabel.textColor = [UIColor colorWithHex:0x9c9c9c];
         [self.contentView addSubview:_timeLabel];
-        
-        _contentLabel = [[TTTAttributedLabel alloc] initWithFrame:CGRectZero];
-        _contentLabel.font = [UIFont systemFontOfSize:13];
-        _contentLabel.textColor = [UIColor colorWithHex:0x9c9c9c];
-        _contentLabel.numberOfLines = 0;
-        _contentLabel.lineBreakMode = NSLineBreakByWordWrapping;
-        _contentLabel.lineSpacing = kLineSpace;
-        [self.contentView addSubview:_contentLabel];
-        
-        _separator = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 1)];
-        _separator.backgroundColor = COMMON_SEPARATOR_COLOR;
-        [self.contentView addSubview:_separator];
     }
     return self;
 }
@@ -93,14 +100,19 @@ static CGFloat kLineSpace = 7;
 
 - (void)layoutSubviews {
     [super layoutSubviews];
-    _avatar.frame = CGRectMake(24, 11, 35, 35);
+    _contentLabel.top = 24;
+    _contentLabel.left = 24;
+    _contentLabel.right = self.right -24;
+    _separator.bottom = _contentLabel.bottom+12;
+    _avatar.top = _separator.bottom +12;
+    _avatar.left = 24;
+    _avatar.size = CGSizeMake(45, 45);
     _nameLabel.centerY = _avatar.centerY;
-    _nameLabel.left = _avatar.right + 8;
+    _nameLabel.left = _avatar.right + 12;
     _timeLabel.centerY = _nameLabel.centerY;
-    _timeLabel.right = SCREEN_WIDTH - 27;
-    _contentLabel.left = 27;
-    _contentLabel.top = _avatar.bottom + 12;
-    _separator.bottom = self.height - 1;
+    _timeLabel.right = SCREEN_WIDTH - 24;
+    _backView.top = _contentLabel.top - 12;
+    _backView.height = _contentLabel.height+12+12+1+12+45+12;
 }
 
 + (CGFloat)calculateCellHeightWithText:(NSString *)text {
@@ -109,7 +121,7 @@ static CGFloat kLineSpace = 7;
     NSDictionary *attrs = @{ NSFontAttributeName : [UIFont systemFontOfSize:13],
                              NSParagraphStyleAttributeName : style};
     CGRect rect = [text boundingRectWithSize:CGSizeMake(SCREEN_WIDTH - 54, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil];
-    return 11 + 35 + 12 + 14 + rect.size.height;
+    return 12+rect.size.height+12+1+12+45+12+12;
 }
 
 @end
