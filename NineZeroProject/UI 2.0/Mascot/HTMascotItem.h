3c2a45a2d7fa02d34e3ffb58f9fe39303e23cd9d
SinLemon <shirokaras@qq.com>
2017-01-05 14:32:23 +0800

    Update - Gif

---
+++ NineZeroProject/UI 2.0/Mascot/HTMascotItem.h
@@ -0,0 +1,30 @@
+//
+//  HTMascotItem.h
+//  NineZeroProject
+//
+//  Created by HHHHTTTT on 16/1/24.
+//  Copyright © 2016年 HHHHTTTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "YYAnimatedImageView.h"
+
+/**
+ *  @brief 小零仔
+ */
+@class HTMascot;
+@interface HTMascotItem : YYAnimatedImageView
+@property (nonatomic, assign) NSInteger index;
+@property (nonatomic, strong) HTMascot *mascot;
+/**
+ *  @brief 播放几号动画，支持2，3，4号三种动画
+ *  @param number 动画编号，支持2，3，4
+ */
+- (void)playAnimatedNumber:(NSInteger)number;
+
+/**
+ *  @brief 停止播放动画
+ */
+- (void)stopAnyAnimation;
+
+@end
