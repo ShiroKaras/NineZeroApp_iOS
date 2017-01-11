896c9ddbce7f20368939a55824e3468cd317119f
SinLemon <shirokaras@qq.com>
2017-01-11 12:10:31 +0800

    Update - Ticket label text font size

--- NineZeroProject/UI 2.0/Question/SKTicketView.m
+++ NineZeroProject/UI 2.0/Question/SKTicketView.m
@@ -43,33 +43,33 @@
     UILabel *ticketTitleLabel_shadow = [UILabel new];
     ticketTitleLabel_shadow.text = self.ticket.title;
     ticketTitleLabel_shadow.textColor = [UIColor blackColor];
-    ticketTitleLabel_shadow.font = PINGFANG_FONT_OF_SIZE(12);
+    ticketTitleLabel_shadow.font = PINGFANG_FONT_OF_SIZE(ROUND_WIDTH_FLOAT(12));
     ticketTitleLabel_shadow.numberOfLines = 2;
     [self addSubview:ticketTitleLabel_shadow];
     [ticketTitleLabel_shadow mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(@14);
         make.left.equalTo(@13);
-        make.width.equalTo(@163);
-        make.height.equalTo(@40);
+        make.width.equalTo(ROUND_WIDTH(163));
+        make.height.equalTo(ROUND_HEIGHT(40));
     }];
     
     UILabel *ticketTitleLabel = [UILabel new];
     ticketTitleLabel.text = self.ticket.title;
     ticketTitleLabel.textColor = [UIColor whiteColor];
-    ticketTitleLabel.font = PINGFANG_FONT_OF_SIZE(12);
+    ticketTitleLabel.font = PINGFANG_FONT_OF_SIZE(ROUND_WIDTH_FLOAT(12));
     ticketTitleLabel.numberOfLines = 2;
     [self addSubview:ticketTitleLabel];
     [ticketTitleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(@13);
         make.left.equalTo(@12);
-        make.width.equalTo(@163);
-        make.height.equalTo(@40);
+        make.width.equalTo(ROUND_WIDTH(163));
+        make.height.equalTo(ROUND_HEIGHT(40));
     }];
     
     UILabel *exchangeCodeLabel_shadow = [UILabel new];
     exchangeCodeLabel_shadow.text = [NSString stringWithFormat:@"唯一兑换码 %@",self.ticket.code];
     exchangeCodeLabel_shadow.textColor = [UIColor blackColor];
-    exchangeCodeLabel_shadow.font = PINGFANG_FONT_OF_SIZE(10);
+    exchangeCodeLabel_shadow.font = PINGFANG_FONT_OF_SIZE(ROUND_WIDTH_FLOAT(10));
     [exchangeCodeLabel_shadow sizeToFit];
     [self addSubview:exchangeCodeLabel_shadow];
     [exchangeCodeLabel_shadow mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -80,7 +80,7 @@
     UILabel *exchangeCodeLabel = [UILabel new];
     exchangeCodeLabel.text = [NSString stringWithFormat:@"唯一兑换码 %@",self.ticket.code];
     exchangeCodeLabel.textColor = [UIColor whiteColor];
-    exchangeCodeLabel.font = PINGFANG_FONT_OF_SIZE(10);
+    exchangeCodeLabel.font = PINGFANG_FONT_OF_SIZE(ROUND_WIDTH_FLOAT(10));
     [exchangeCodeLabel sizeToFit];
     [self addSubview:exchangeCodeLabel];
     [exchangeCodeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -98,7 +98,7 @@
     UILabel *timeLimitLabal_shadow = [UILabel new];
     timeLimitLabal_shadow.text = [NSString stringWithFormat:@"有效期至%@",confromTimespStr];
     timeLimitLabal_shadow.textColor = [UIColor blackColor];
-    timeLimitLabal_shadow.font = PINGFANG_FONT_OF_SIZE(10);
+    timeLimitLabal_shadow.font = PINGFANG_FONT_OF_SIZE(ROUND_WIDTH_FLOAT(10));
     [timeLimitLabal_shadow sizeToFit];
     [self addSubview:timeLimitLabal_shadow];
     [timeLimitLabal_shadow mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -109,7 +109,7 @@
     UILabel *timeLimitLabal = [UILabel new];
     timeLimitLabal.text = [NSString stringWithFormat:@"有效期至%@",confromTimespStr];
     timeLimitLabal.textColor = [UIColor whiteColor];
-    timeLimitLabal.font = PINGFANG_FONT_OF_SIZE(10);
+    timeLimitLabal.font = PINGFANG_FONT_OF_SIZE(ROUND_WIDTH_FLOAT(10));
     [timeLimitLabal sizeToFit];
     [self addSubview:timeLimitLabal];
     [timeLimitLabal mas_makeConstraints:^(MASConstraintMaker *make) {
