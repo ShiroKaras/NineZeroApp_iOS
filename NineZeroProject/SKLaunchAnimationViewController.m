1b4cf454a8dd5a7fa513477e340745015dd15b17
SinLemon <shirokaras@qq.com>
2017-02-09 18:51:50 +0800

    Update

--- NineZeroProject/SKLaunchAnimationViewController.m
+++ NineZeroProject/SKLaunchAnimationViewController.m
@@ -14,120 +14,120 @@
 @property (nonatomic, strong) UIButton *skipButton;
 @property (nonatomic, strong) UIButton *enterButton;
 
-@property (strong, nonatomic) AVPlayer      *player;
+@property (strong, nonatomic) AVPlayer *player;
 @property (strong, nonatomic) AVPlayerLayer *playerLayer;
-@property (strong, nonatomic) AVPlayerItem  *playerItem;
+@property (strong, nonatomic) AVPlayerItem *playerItem;
 
 @end
 
 @implementation SKLaunchAnimationViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blackColor];
-    
-    [self createUI];
-    [self createVideo];
-    
-    [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(showSkipButton) userInfo:nil repeats:NO];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playItemDidPlayToEndTime:) name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
+	[super viewDidLoad];
+	self.view.backgroundColor = [UIColor blackColor];
+
+	[self createUI];
+	[self createVideo];
+
+	[NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(showSkipButton) userInfo:nil repeats:NO];
+
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playItemDidPlayToEndTime:) name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
+	[super viewWillAppear:animated];
+	[[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
-//    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
+	[super viewWillDisappear:animated];
+	//    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+	[super didReceiveMemoryWarning];
 }
 
-- (void)dealloc
-{
-    self.view = nil;
+- (void)dealloc {
+	self.view = nil;
 }
 
 #pragma mark - Acitons
 - (void)createUI {
-    _skipButton = [UIButton new];
-    [_skipButton addTarget:self action:@selector(onClickSkipButton:) forControlEvents:UIControlEventTouchUpInside];
-    [_skipButton setImage:[UIImage imageNamed:@"btn_trailer_skip"] forState:UIControlStateNormal];
-    [_skipButton sizeToFit];
-    _skipButton.alpha = 0;
-    [self.view addSubview:_skipButton];
-    
-    _enterButton = [UIButton new];
-    [_enterButton addTarget:self action:@selector(onClickEnterButton:) forControlEvents:UIControlEventTouchUpInside];
-    [_enterButton setImage:[UIImage imageNamed:@"btn_trailer_enter"] forState:UIControlStateNormal];
-    [_enterButton sizeToFit];
-    _enterButton.alpha = 0;
-    [self.view addSubview:_enterButton];
-    
-    [_skipButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.right.equalTo(self.view.mas_right).offset(-25);
-        make.bottom.equalTo(self.view.mas_bottom).offset(-25);
-    }];
-    
-    [_enterButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerY.equalTo(self.view.mas_centerY);
-        make.left.equalTo(self.view.mas_left).offset(31);
-    }];
+	_skipButton = [UIButton new];
+	[_skipButton addTarget:self action:@selector(onClickSkipButton:) forControlEvents:UIControlEventTouchUpInside];
+	[_skipButton setImage:[UIImage imageNamed:@"btn_trailer_skip"] forState:UIControlStateNormal];
+	[_skipButton sizeToFit];
+	_skipButton.alpha = 0;
+	[self.view addSubview:_skipButton];
+
+	_enterButton = [UIButton new];
+	[_enterButton addTarget:self action:@selector(onClickEnterButton:) forControlEvents:UIControlEventTouchUpInside];
+	[_enterButton setImage:[UIImage imageNamed:@"btn_trailer_enter"] forState:UIControlStateNormal];
+	[_enterButton sizeToFit];
+	_enterButton.alpha = 0;
+	[self.view addSubview:_enterButton];
+
+	[_skipButton mas_makeConstraints:^(MASConstraintMaker *make) {
+	    make.right.equalTo(self.view.mas_right).offset(-25);
+	    make.bottom.equalTo(self.view.mas_bottom).offset(-25);
+	}];
+
+	[_enterButton mas_makeConstraints:^(MASConstraintMaker *make) {
+	    make.centerY.equalTo(self.view.mas_centerY);
+	    make.left.equalTo(self.view.mas_left).offset(31);
+	}];
 }
 
 - (void)createVideo {
-    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"trailer_video" ofType:@"mp4"];
-    NSURL *sourceMovieURL = [NSURL fileURLWithPath:filePath];
-    
-    AVAsset *movieAsset	= [AVURLAsset URLAssetWithURL:sourceMovieURL options:nil];
-    _playerItem = [AVPlayerItem playerItemWithAsset:movieAsset];
-    _player = [AVPlayer playerWithPlayerItem:_playerItem];
-    _playerLayer = [AVPlayerLayer playerLayerWithPlayer:_player];
-    _playerLayer.frame = CGRectMake(0, 0, self.view.width, self.view.height);
-    _playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-    [self.view.layer insertSublayer:_playerLayer atIndex:0];
-    
-    [_player play];
+	NSString *filePath = [[NSBundle mainBundle] pathForResource:@"trailer_video" ofType:@"mp4"];
+	NSURL *sourceMovieURL = [NSURL fileURLWithPath:filePath];
+
+	AVAsset *movieAsset = [AVURLAsset URLAssetWithURL:sourceMovieURL options:nil];
+	_playerItem = [AVPlayerItem playerItemWithAsset:movieAsset];
+	_player = [AVPlayer playerWithPlayerItem:_playerItem];
+	_playerLayer = [AVPlayerLayer playerLayerWithPlayer:_player];
+	_playerLayer.frame = CGRectMake(0, 0, self.view.width, self.view.height);
+	_playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+	[self.view.layer insertSublayer:_playerLayer atIndex:0];
+
+	[_player play];
 }
 
 - (void)showSkipButton {
-    [UIView animateWithDuration:0.3 animations:^{
-        _skipButton.alpha = 1.0;
-    }];
+	[UIView animateWithDuration:0.3
+			 animations:^{
+			     _skipButton.alpha = 1.0;
+			 }];
 }
 
 - (void)showEnterButton {
-    [UIView animateWithDuration:0.3 animations:^{
-        _enterButton.alpha = 1.0;
-        _skipButton.alpha = 0;
-    }];
+	[UIView animateWithDuration:0.3
+			 animations:^{
+			     _enterButton.alpha = 1.0;
+			     _skipButton.alpha = 0;
+			 }];
 }
 
-- (void)onClickSkipButton:(UIButton*)sender {
-    if (self.didSelectedEnter) {
-        self.didSelectedEnter();
-    }
+- (void)onClickSkipButton:(UIButton *)sender {
+	if (self.didSelectedEnter) {
+		self.didSelectedEnter();
+	}
 }
 
-
-- (void)onClickEnterButton:(UIButton*)sender {
-    if (self.didSelectedEnter) {
-        self.didSelectedEnter();
-    }
+- (void)onClickEnterButton:(UIButton *)sender {
+	if (self.didSelectedEnter) {
+		self.didSelectedEnter();
+	}
 }
 
 #pragma mark - Notification
 
 - (void)playItemDidPlayToEndTime:(NSNotification *)notification {
-    if ([notification.object isEqual:self.playerItem]) {
-        //显示进入按钮
-        [self showEnterButton];
-    }
+	if ([notification.object isEqual:self.playerItem]) {
+		//显示进入按钮
+		[self showEnterButton];
+	}
 }
 
 @end
