b731eb914fd97186d2f19b217cecef332d0d5b0f
SinLemon <shirokaras@qq.com>
2017-05-04 15:02:43 +0800

    Update - Task detail resize

--- NineZeroProject/UI/Task/NZTaskDetailViewController.m
+++ NineZeroProject/UI/Task/NZTaskDetailViewController.m
@@ -17,8 +17,6 @@
 @property (nonatomic, strong) UIImageView   *indicatorLine;
 @property (nonatomic, strong) UIButton      *button1;
 @property (nonatomic, strong) UIButton      *button2;
-@property (nonatomic, strong) UIScrollView  *scrollView1;
-@property (nonatomic, strong) UIScrollView  *scrollView2;
 @property (nonatomic, strong) UIButton      *addTaskButton;
 //@property (nonatomic, strong) UIView        *tipsBackView;
 @property (nonatomic, assign) BOOL          isAddTaskList;
@@ -43,99 +41,33 @@
 - (void)createUI {
     WS(weakSelf);
     self.view.backgroundColor = COMMON_BG_COLOR;
+    self.automaticallyAdjustsScrollViewInsets = false;
     
-    _titleImageView = [UIImageView new];
-    _titleImageView.image = [UIImage imageNamed:@"img_monday_music_cover_default"];
-    _titleImageView.layer.masksToBounds = YES;
-    _titleImageView.contentMode = UIViewContentModeScaleAspectFill;
-    [self.view addSubview:_titleImageView];
-    [_titleImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.size.mas_equalTo(CGSizeMake(weakSelf.view.width, weakSelf.view.width/320*240));
-        make.top.equalTo(@0);
-        make.centerX.equalTo(weakSelf.view);
-    }];
-    
-    //TabView
-    _tabBackView = [UIView new];
-    _tabBackView.backgroundColor = [UIColor clearColor];
-    [self.view addSubview:_tabBackView];
-    [_tabBackView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.size.mas_equalTo(CGSizeMake(weakSelf.view.width, 54));
-        make.centerX.equalTo(weakSelf.view);
-        make.top.equalTo(_titleImageView.mas_bottom);
-    }];
-    
-    _button1 = [UIButton new];
-    [_button1 addTarget:self action:@selector(didClickButton1:) forControlEvents:UIControlEventTouchUpInside];
-    [_button1 setBackgroundImage:[UIImage imageNamed:@"btn_taskpage_scene_highlight"] forState:UIControlStateNormal];
-    [_button1 setBackgroundImage:[UIImage imageNamed:@"btn_taskpage_scene_highlight"] forState:UIControlStateHighlighted];
-    [self.view addSubview:_button1];
-    [_button1 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.size.mas_equalTo(CGSizeMake(27, 27));
-        make.centerY.equalTo(_tabBackView);
-        make.left.equalTo(@82);
-    }];
-
-    _button2 = [UIButton new];
-    [_button2 addTarget:self action:@selector(didClickButton2:) forControlEvents:UIControlEventTouchUpInside];
-    [_button2 setBackgroundImage:[UIImage imageNamed:@"btn_taskpage_arrest"] forState:UIControlStateNormal];
-    [_button2 setBackgroundImage:[UIImage imageNamed:@"btn_taskpage_arrest_highlight"] forState:UIControlStateHighlighted];
-    [self.view addSubview:_button2];
-    [_button2 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.size.mas_equalTo(CGSizeMake(27, 27));
-        make.centerY.equalTo(_tabBackView);
-        make.right.equalTo(_tabBackView).offset(-82);
-    }];
-
-    UIView *underLine = [UIView new];
-    [_tabBackView addSubview:underLine];
-    [underLine mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.size.mas_equalTo(CGSizeMake(self.view.width, 1));
-        make.centerX.equalTo(_tabBackView);
-        make.bottom.equalTo(_tabBackView);
-    }];
-    
-    _indicatorLine = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"img_taskdetailpage_choose"]];
-    [_tabBackView addSubview:_indicatorLine];
-    [_indicatorLine mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.equalTo(_button1);
-        make.bottom.equalTo(underLine.mas_top);
-    }];
-
-    _scrollView = [UIScrollView new];
+    //MainView
+    _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.width, self.view.height-49)];
     _scrollView.delegate = self;
     _scrollView.backgroundColor = [UIColor clearColor];
     _scrollView.bounces = NO;
-    _scrollView.pagingEnabled = YES;
 //    _scrollView.showsHorizontalScrollIndicator = NO;
     [self.view addSubview:_scrollView];
-    [_scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(_tabBackView.mas_bottom);
-        make.bottom.equalTo(weakSelf.view);
-        make.width.equalTo(weakSelf.view);
-        make.centerX.equalTo(weakSelf.view);
-    }];
-
-    [self.view layoutIfNeeded];
-    _scrollView.contentSize = CGSizeMake(self.view.width*2, _scrollView.height);
-
-    _scrollView1 = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.width, _scrollView.height)];
-    _scrollView1.contentSize = CGSizeMake(self.view.width, 1000);
-    _scrollView1.backgroundColor = [UIColor clearColor];
-    _scrollView1.bounces = NO;
-    [_scrollView addSubview:_scrollView1];
     
-    NZTaskDetailView *detailView = [[NZTaskDetailView alloc] initWithFrame:CGRectMake(0, 0, self.view.width, 1000) withModel:nil];
-    detailView.frame = CGRectMake(0, 0, self.view.width, detailView.viewHeight);
-    [_scrollView1 addSubview:detailView];
-    _scrollView1.contentSize = CGSizeMake(self.view.width, detailView.viewHeight+16+40);
+    _titleImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.width, self.view.width/320*240)];
+    _titleImageView.image = [UIImage imageNamed:@"img_monday_music_cover_default"];
+    _titleImageView.layer.masksToBounds = YES;
+    _titleImageView.contentMode = UIViewContentModeScaleAspectFill;
+    [_scrollView addSubview:_titleImageView];
+    
+    NZTaskDetailView *detailView = [[NZTaskDetailView alloc] initWithFrame:CGRectMake(0, _titleImageView.bottom, self.view.width, 1000) withModel:nil];
+    [_scrollView addSubview:detailView];
+    
+    _scrollView.contentSize = CGSizeMake(self.view.width, detailView.viewHeight+16+40+weakSelf.view.width/320*240);
     
     UIView *bottomView = [UIView new];
-    bottomView.backgroundColor = COMMON_SEPARATOR_COLOR;
+    bottomView.backgroundColor = COMMON_TITLE_BG_COLOR;
     [self.view addSubview:bottomView];
     [bottomView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.width.equalTo(weakSelf.view);
-        make.height.equalTo(@40);
+        make.height.equalTo(@49);
         make.centerX.equalTo(weakSelf.view);
         make.bottom.equalTo(weakSelf.view);
     }];
@@ -238,34 +170,34 @@
 #pragma mark - UIScrollViewDelegate
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-    if (scrollView == _scrollView) {
-        NSLog(@"%lf", scrollView.contentOffset.x);
-        //得到图片移动相对原点的坐标
-        CGPoint point = scrollView.contentOffset;
-        
-        if (point.x > 2 * (SCREEN_WIDTH)) {
-            point.x = (SCREEN_WIDTH)* 2;
-            scrollView.contentOffset = point;
-        }
-    }
+//    if (scrollView == _scrollView) {
+//        NSLog(@"%lf", scrollView.contentOffset.x);
+//        //得到图片移动相对原点的坐标
+//        CGPoint point = scrollView.contentOffset;
+//        
+//        if (point.x > 2 * (SCREEN_WIDTH)) {
+//            point.x = (SCREEN_WIDTH)* 2;
+//            scrollView.contentOffset = point;
+//        }
+//    }
 }
 
 - (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
-    if (scrollView == _scrollView) {
-        //根据图片坐标判断页数
-        NSInteger index = round(targetContentOffset->x / (SCREEN_WIDTH));
-        NSLog(@"%ld", (long)index);
-        switch (index) {
-            case 0:
-                [self didClickButton1:nil];
-                break;
-            case 1:
-                [self didClickButton2:nil];
-                break;
-            default:
-                break;
-        }
-    }
+//    if (scrollView == _scrollView) {
+//        //根据图片坐标判断页数
+//        NSInteger index = round(targetContentOffset->x / (SCREEN_WIDTH));
+//        NSLog(@"%ld", (long)index);
+//        switch (index) {
+//            case 0:
+//                [self didClickButton1:nil];
+//                break;
+//            case 1:
+//                [self didClickButton2:nil];
+//                break;
+//            default:
+//                break;
+//        }
+//    }
 }
 
 @end
